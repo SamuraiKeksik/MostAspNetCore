cfb71435b36d3ed59891224e3df68fcc23bc5354
SamuraiKeksik <samuraikeksik@gmail.com>
2024-10-02 19:14:57 +0300

    Добавил контроллер Buildings и представления к нему. Готовы представления: index, create и edit

---
+++ MostAspNetCore/Controllers/BuildingsController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MostAspNetCore.Data;
+using MostLib;
+
+namespace MostAspNetCore.Controllers
+{
+    public class BuildingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public BuildingsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Buildings
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Здания";
+            return View(await _context.Buildings.ToListAsync());
+        }
+
+        public async Task<IActionResult> Stores()
+        {
+            ViewData["Title"] = "Магазины";
+            return View("Index", await _context.Buildings.Where(b => b.BuildingTypeId == MostLib.Enums.BuildingType.Store).ToListAsync());
+        }
+
+        public async Task<IActionResult> Warehouses()
+        {
+            ViewData["Title"] = "Склады";
+            return View("Index", await _context.Buildings.Where(b => b.BuildingTypeId == MostLib.Enums.BuildingType.Warehouse).ToListAsync());
+        }
+
+
+        // GET: Buildings/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var building = await _context.Buildings
+                .FirstOrDefaultAsync(m => m.BuildingId == id);
+            if (building == null)
+            {
+                return NotFound();
+            }
+
+            return View(building);
+        }
+
+        // GET: Buildings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Buildings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Building building)
+        {
+            ModelState.Remove("BuildingId");
+            ModelState.Remove("User");      //Реализовать проверку на пользователя
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                    return Unauthorized();
+
+                building.BuildingId = Guid.NewGuid();
+                building.User = user;
+                _context.Add(building);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(building);
+        }
+
+        // GET: Buildings/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var building = await _context.Buildings.FindAsync(id);
+            if (building == null)
+            {
+                return NotFound();
+            }
+            return View(building);
+        }
+
+        // POST: Buildings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("BuildingId,Address,Coordinates,BuildingName,BuildingTypeId,Comment")] Building building)
+        {
+            ModelState.Remove("User");      //Реализовать проверку на пользователя
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(building);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BuildingExists(building.BuildingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(building);
+        }
+
+        // GET: Buildings/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var building = await _context.Buildings
+                .FirstOrDefaultAsync(m => m.BuildingId == id);
+            if (building == null)
+            {
+                return NotFound();
+            }
+
+            return View(building);
+        }
+
+        // POST: Buildings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var building = await _context.Buildings.FindAsync(id);
+            if (building != null)
+            {
+                _context.Buildings.Remove(building);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BuildingExists(Guid id)
+        {
+            return _context.Buildings.Any(e => e.BuildingId == id);
+        }
+    }
+}
--- MostLib/Building.cs
+++ MostLib/Building.cs
@@ -2,6 +2,7 @@
 using MostLib.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -16,20 +17,25 @@ namespace MostLib
 
         [Required(ErrorMessage = "Не указан адрес здания!")]
         [MaxLength(100, ErrorMessage = "Максимальная длина адреса - 100!")]
+        [DisplayName("Адрес")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Не указаны координаты здания!")]
+        [DisplayName("Координаты")]
         public string Coordinates { get; set; }
 
         [Required(ErrorMessage = "Не указано название здания!")]
         [MaxLength(100, ErrorMessage = "Максимальная длина названия - 100!")]
+        [DisplayName("Название здания")]
         public string BuildingName { get; set; }   //Название здания, например - Магазин "Магнит"
 
         [Required(ErrorMessage = "Не указан тип здания!")]
+        [DisplayName("Тип здания")]
         public BuildingType BuildingTypeId { get; set; }
 
         [MaxLength(500, ErrorMessage = "Максимальная длина комментария - 500!")]
-        public string? Comment { get; set; }   //Комментарий к строению, например - с какой стороны подъежать
+        [DisplayName("Примечание")]
+        public string? Comment { get; set; }   //Примечание к строению, например - с какой стороны подъежать
 
         [Required]
         public IdentityUser User { get; set; }  //Содержит пользователя, который добавил здание
